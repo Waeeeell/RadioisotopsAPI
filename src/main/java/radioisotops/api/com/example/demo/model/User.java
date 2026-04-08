b1977561d0b27b4d3702dcf9ccd645734d88d0b7
Fedeerich <marcosgongora1000@gmail.com>
2026-04-08 19:04:09 +0200

    Modificado para que la pestaña de configuracion funcione y se guarde

--- src/main/java/radioisotops/api/com/example/demo/model/User.java
+++ src/main/java/radioisotops/api/com/example/demo/model/User.java
@@ -38,23 +38,24 @@ public class User {
     @Column(name = "zona_horaria", columnDefinition = "VARCHAR(100) DEFAULT 'Europa/Madrid (CET)'")
     private String zonaHoraria = "Europa/Madrid (CET)";
 
+    // Cambiamos de 'boolean' a 'Boolean' para permitir nulos de registros antiguos
     @Column(name = "notif_bateria")
-    private boolean notifBateria = true;
+    private Boolean notifBateria = true;
 
     @Column(name = "notif_desconexion")
-    private boolean notifDesconexion = true;
+    private Boolean notifDesconexion = true;
 
     @Column(name = "notif_resumen")
-    private boolean notifResumen = false;
+    private Boolean notifResumen = false;
 
     @Column(name = "notif_radiacion")
-    private boolean notifRadiacion = true;
+    private Boolean notifRadiacion = true;
 
     @Column(name = "notif_vitales")
-    private boolean notifVitales = true;
+    private Boolean notifVitales = true;
 
     @Column(name = "notif_sincro")
-    private boolean notifSincro = false;
+    private Boolean notifSincro = false;
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     @JsonManagedReference
@@ -110,28 +111,28 @@ public class User {
     public Patient getPatient() { return patient; }
     public void setPatient(Patient patient) { this.patient = patient; }
 
-    // Getters y Setters de Preferencias
     public String getIdioma() { return idioma; }
     public void setIdioma(String idioma) { this.idioma = idioma; }
 
     public String getZonaHoraria() { return zonaHoraria; }
     public void setZonaHoraria(String zonaHoraria) { this.zonaHoraria = zonaHoraria; }
 
-    public boolean isNotifBateria() { return notifBateria; }
-    public void setNotifBateria(boolean notifBateria) { this.notifBateria = notifBateria; }
+    // Actualizamos los Getters/Setters para usar Boolean
+    public Boolean getNotifBateria() { return notifBateria != null ? notifBateria : true; }
+    public void setNotifBateria(Boolean notifBateria) { this.notifBateria = notifBateria; }
 
-    public boolean isNotifDesconexion() { return notifDesconexion; }
-    public void setNotifDesconexion(boolean notifDesconexion) { this.notifDesconexion = notifDesconexion; }
+    public Boolean getNotifDesconexion() { return notifDesconexion != null ? notifDesconexion : true; }
+    public void setNotifDesconexion(Boolean notifDesconexion) { this.notifDesconexion = notifDesconexion; }
 
-    public boolean isNotifResumen() { return notifResumen; }
-    public void setNotifResumen(boolean notifResumen) { this.notifResumen = notifResumen; }
+    public Boolean getNotifResumen() { return notifResumen != null ? notifResumen : false; }
+    public void setNotifResumen(Boolean notifResumen) { this.notifResumen = notifResumen; }
 
-    public boolean isNotifRadiacion() { return notifRadiacion; }
-    public void setNotifRadiacion(boolean notifRadiacion) { this.notifRadiacion = notifRadiacion; }
+    public Boolean getNotifRadiacion() { return notifRadiacion != null ? notifRadiacion : true; }
+    public void setNotifRadiacion(Boolean notifRadiacion) { this.notifRadiacion = notifRadiacion; }
 
-    public boolean isNotifVitales() { return notifVitales; }
-    public void setNotifVitales(boolean notifVitales) { this.notifVitales = notifVitales; }
+    public Boolean getNotifVitales() { return notifVitales != null ? notifVitales : true; }
+    public void setNotifVitales(Boolean notifVitales) { this.notifVitales = notifVitales; }
 
-    public boolean isNotifSincro() { return notifSincro; }
-    public void setNotifSincro(boolean notifSincro) { this.notifSincro = notifSincro; }
+    public Boolean getNotifSincro() { return notifSincro != null ? notifSincro : false; }
+    public void setNotifSincro(Boolean notifSincro) { this.notifSincro = notifSincro; }
 }
